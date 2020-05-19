93b0b192203994896f50d10d89f4e4d66fedeef9
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-05-19 17:34:00 +0200

    Show sign in or out notification with new useAuth

--- src/index.tsx
+++ src/index.tsx
@@ -119,23 +119,34 @@ const startApp = async () => {
         }
       }, [updateAvailable]);
 
-      useAuth((authenticated: boolean) => {
-        if (authenticated) {
-          alert.show(
-            {
-              title: "You are now logged in",
-            },
-            { type: "success" }
-          );
-        } else {
-          alert.show(
-            {
-              title: "You are now logged out",
-            },
-            { type: "success" }
-          );
+      const { authenticated } = useAuth();
+      const [prevAuthenticated, setPrevAuthenticated] = React.useState<
+        boolean | undefined
+      >();
+
+      React.useEffect(() => {
+        if (prevAuthenticated !== undefined && authenticated !== undefined) {
+          if (!prevAuthenticated && authenticated) {
+            alert.show(
+              {
+                title: "You are now logged in",
+              },
+              { type: "success" }
+            );
+          } else if (prevAuthenticated && !authenticated) {
+            alert.show(
+              {
+                title: "You are now logged out",
+              },
+              { type: "success" }
+            );
+          }
+          setPrevAuthenticated(authenticated);
+        } else if (authenticated !== undefined) {
+          setPrevAuthenticated(authenticated);
         }
-      });
+      }, [authenticated]);
+
       return null;
     };
 
