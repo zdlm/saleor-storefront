361bf7e4d575db9026f0ebeceb74c8419b387969
Leo zhang <leozhang@Leos-MacBook-Pro.local>
2020-08-24 20:42:16 +0200

    change logo

--- src/core/config.ts
+++ src/core/config.ts
@@ -24,22 +24,22 @@ export const STATIC_PAGES = [
 export const SOCIAL_MEDIA = [
   {
     ariaLabel: "facebook",
-    href: "https://www.facebook.com/mirumeelabs/",
+    href: "https://www.facebook.com/norsden.aps.39/",
     path: require("../images/facebook-icon.svg"),
   },
   {
     ariaLabel: "instagram",
-    href: "https://www.instagram.com/mirumeelabs/",
+    href: "https://www.instagram.com/norsdenaps/",
     path: require("../images/instagram-icon.svg"),
   },
   {
     ariaLabel: "twitter",
-    href: "https://twitter.com/getsaleor",
+    href: "https://twitter.com/Norsden1",
     path: require("../images/twitter-icon.svg"),
   },
   {
     ariaLabel: "youtube",
-    href: "https://www.youtube.com/channel/UCg_ptb-U75e7BprLCGS4s1g/videos",
+    href: "https://www.youtube.com/channel/UCrwTkrS0iLhvDmbQ5tn0_pA/",
     path: require("../images/youtube-icon.svg"),
   },
 ];
